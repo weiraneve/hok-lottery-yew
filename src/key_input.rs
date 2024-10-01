efe7c53898ae7fc67b5b224d36cce25da438665c
weiraneve <907221539@qq.com>
2024-10-01 12:22:44 +0800

    chore: update port

--- src/key_input.rs
+++ src/key_input.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::result_table::{ResultTable, PickHistory};
 
-const BASE_URL: &str = "http://localhost:8080";
+const BASE_URL: &str = "http://localhost:8000";
 
 #[derive(Clone, PartialEq, Serialize, Deserialize)]
 struct ApiPickHistory {
